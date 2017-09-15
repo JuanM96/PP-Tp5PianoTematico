56d9375acdded5d2893106a719cca3ac21a36149
Juan Ignacio Murcia <juanignaciomurcia@gmail.com>
2017-09-15 04:59:13 -0300

    icon & splash cambiados + login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+//import { RegistroPage } from '../pages/registro/registro';
+
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from '../providers/auth-service/auth-service';
 
 @NgModule({
   declarations: [
@@ -17,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
     TabsPage
   ],
   imports: [
@@ -29,12 +35,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
+    //RegistroPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
   ]
 })
 export class AppModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
+import { LoginPage } from '../login/login';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
@@ -9,9 +10,10 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = LoginPage;
+  tab2Root = HomePage;
+  tab3Root = AboutPage;
+  tab4Root = ContactPage;
 
   constructor() {
 
